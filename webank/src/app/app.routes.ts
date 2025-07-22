fadef5a679eb91ec5e60a27c83310b94ae555e8f
HOME <HOME@DESKTOP-VUVTT19>
2025-07-22 13:00:02 +0530

    Dashboard Changes

--- webank/src/app/app.routes.ts
+++ webank/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
 
-   {path:'', redirectTo:'/user', pathMatch:'full'},
+   {path:'', redirectTo:'/dashboard', pathMatch:'full'},
     {path:'Admin', component:AdminComponent},
+    {path:'dashboard', component:DashboardComponent},
 
     {path:'user', loadChildren: () =>
       import('./user/user.module').then(m => m.UserModule)}
